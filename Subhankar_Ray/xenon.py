789f871d271371e8b2dd0a34d2b0e23838361d49
Subhankar Ray <subhankarraypp32@gmail.com>
2023-09-30 23:03:14 +0530

    Minor-Bug-Fix

--- Subhankar_Ray/xenon.py
+++ Subhankar_Ray/xenon.py
@@ -8,17 +8,17 @@ win=Tk()
 class Preconfig:
     def auto_config_tk(self):
     # Check if python3-tk is already installed
-    try:
-        subprocess.run(["python3", "-c", "import tkinter"], check=True)
-        print("python3-tk is already installed.")
-    except subprocess.CalledProcessError:
-        # If an error occurred, it means tkinter is not installed, so we'll try to install it
         try:
-            subprocess.run(["sudo", "apt", "update"], check=True)
-            subprocess.run(["sudo", "apt", "install", "-y", "python3-tk"], check=True)
-            print("python3-tk has been installed.")
+            subprocess.run(["python3", "-c", "import tkinter"], check=True)
+            print("python3-tk is already installed.")
         except subprocess.CalledProcessError:
-            print("Failed to install python3-tk. Please check your internet connection or try manually.")
+            # If an error occurred, it means tkinter is not installed, so we'll try to install it
+            try:
+                subprocess.run(["sudo", "apt", "update"], check=True)
+                subprocess.run(["sudo", "apt", "install", "-y", "python3-tk"], check=True)
+                print("python3-tk has been installed.")
+            except subprocess.CalledProcessError:
+                print("Failed to install python3-tk. Please check your internet connection or try manually.")
 
     
 class Window:
