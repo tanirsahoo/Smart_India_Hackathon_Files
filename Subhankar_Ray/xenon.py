d9bac720b89d051e05375d3da16636ccb4e7db63
Subhankar Ray <subhankarraypp32@gmail.com>
2023-09-30 22:10:33 +0530

    Firewall-Blocking Integrated

--- Subhankar_Ray/xenon.py
+++ Subhankar_Ray/xenon.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from abc import ABC , abstractmethod
+import subprocess
 
 win=Tk()
 
@@ -90,11 +91,47 @@ class ViewWindow(Window,Win_frames):
     def pass_script(self):
         print("Password Policy Script")
 
+    class Policy:
+        def enable_ufw():
+            try:
+                subprocess.run(["sudo", "ufw", "enable"], check=True)
+                print("UFW enabled.")
+            except subprocess.CalledProcessError as e:
+                print(f"Error enabling UFW: {e}")
+
+        def allow_ssh():
+            try:
+                subprocess.run(["sudo", "ufw", "allow", "OpenSSH"], check=True)
+                print("SSH access allowed.")
+            except subprocess.CalledProcessError as e:
+                print(f"Error allowing SSH: {e}")
+
+        def allow_custom_ports(ports):
+            for port in ports:
+                try:
+                    subprocess.run(["sudo", "ufw", "allow", str(port)], check=True)
+                    print(f"Port {port} allowed.")
+                except subprocess.CalledProcessError as e:
+                    print(f"Error allowing port {port}: {e}")
+
+
     def secure_ssh(self):
+        pol = Policy()
+
         print("Secure shell ssh")
+        # Enable UFW
+        pol.enable_ufw()
+
+        # Allow SSH
+        pol.allow_ssh()
+
+        # Define additional ports to allow (e.g., 80 for HTTP, 443 for HTTPS)
+        pol.custom_ports = [80, 443]
+
+        # Allow custom ports
+        pol.allow_custom_ports(custom_ports)
 
 
 if __name__=="__main__":
     AuthWindow().frames_pos()
     win.mainloop()
-
