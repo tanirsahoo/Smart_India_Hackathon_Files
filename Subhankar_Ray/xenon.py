e99b6a280e24bda07dbaf603a0dee71596f9d750
Subhankar Ray <subhankarraypp32@gmail.com>
2023-10-01 14:34:31 +0530

    Authorisation Frame Design Added
    New Frames: View
    New Frames Design: Policy
    
    To be added

--- Subhankar_Ray/xenon.py
+++ Subhankar_Ray/xenon.py
@@ -1,13 +1,16 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from abc import ABC , abstractmethod
 import subprocess
 
-win=Tk()
+win=tk.Tk()
 
 class Preconfig:
     def auto_config_tk(self):
-    # Check if python3-tk is already installed
+        print("Config-Chk")
+        """
+        # Check if python3-tk is already installed
         try:
             subprocess.run(["python3", "-c", "import tkinter"], check=True)
             print("python3-tk is already installed.")
@@ -19,12 +22,18 @@ class Preconfig:
                 print("python3-tk has been installed.")
             except subprocess.CalledProcessError:
                 print("Failed to install python3-tk. Please check your internet connection or try manually.")
+                """
 
-    
-class Window:
+
+
+class ShareVar:
+    def setHeightWidth(self,height,width):
+        self.height=height
+        self.width=width
+
+class Window(ShareVar):
     def __init__(self):
-        self.width=500
-        self.height= 500
+        super().setHeightWidth(500,500)
         self.title="Xenon"
 
     def window_res(self):
@@ -35,18 +44,24 @@ class Window:
         win.configure(bg="#FFFFFF")
 
 
-class Win_frames(ABC):
-    def frames_gen(self, k, bd, wd, ht):
+class Win_frames(ABC,ShareVar):
+    def main_frame_gen(self):
+        self.main_frame=Frame(win ,bg="#808080" ,borderwidth=1 ,relief=SUNKEN ,width=self.width ,height=self.height)
+        self.main_frame.pack(fill = tk.BOTH, expand = True)
+        self.main_frame.pack_propagate(0)
+        self.main_frame.place(anchor = 'center', relx = 0.5, rely = 0.5)
+        
+    def child_frames_gen(self, k, bd, ht, wd, cl):
         flist=[]
-        for i in range(k+1):
-            flist.append(Frame(win, bg="#808080" ,borderwidth=bd, relief=SUNKEN, width=wd, height=ht))
+        for i in range(1,k+1):
+            flist.append(Frame(self.main_frame, bg=cl ,borderwidth=bd, relief=SUNKEN, width=wd, height=ht))
         return flist
 
     @abstractmethod
-    def frames_pos(self):
+    def child_frames_pos(self):
         pass
 
-    def frames_del(self,obj):
+    def child_frames_del(self,obj):
         obj.destroy()
 
 class Win_buttons(ABC):
@@ -57,7 +72,6 @@ class Win_buttons(ABC):
     def button_del(self,obj):
         obj.destroy()
 
-
 class AuthWindow(Window,Win_frames,Win_buttons):
     def __init__(self):
         super().__init__()
@@ -65,16 +79,28 @@ class AuthWindow(Window,Win_frames,Win_buttons):
         self.frames_list=[]
     
 
-    def frames_pos(self):
-        self.frames_list = self.frames_gen(1,1,400,400)
+    def child_frames_pos(self):
+        self.main_frame_gen()
+        self.design_frames_list = self.child_frames_gen(2,1,int(0.2*self.height),self.width, "#008080")
+        self.frames_list = self.child_frames_gen(1,1,300,self.width, "#FFFFFF")
+
+        self.design_frames_list[0].pack(side="top", expand=False)
+        self.design_frames_list[0].pack_propagate(0)
+        
         self.frames_list[0].pack(side="top", expand=False)
+        self.frames_list[0].pack_propagate(0)
+
+        self.design_frames_list[1].pack(side="top", expand=False)
+        self.design_frames_list[1].pack_propagate(0)
+
+
         print("Frame positioned")
         self.button_create(self.frames_list[0] ,"Sign In", "#FFFFFF")
     
     def button_create(self ,f ,t ,bcl):
         btn1 = Button(f ,text=t ,bg=bcl ,command = lambda: self.auth_script())
         btn1.pack()
-        Button(f, text="Delete_btn", bg =bcl, command = lambda: self.button_del(btn1)).pack()
+        btn1.place(anchor = 'center', relx = 0.5, rely = 0.5)
 
     def auth_script(self):
         #Auth-Script
@@ -82,8 +108,8 @@ class AuthWindow(Window,Win_frames,Win_buttons):
         z = 1
         if z:
             print("Authenticated")
-            self.frames_del(self.frames_list[0])
-            ViewWindow().frames_pos()
+            self.child_frames_del(self.frames_list[0])
+            ViewWindow().child_frames_pos()
 
 class ViewWindow(Window,Win_frames):
     def __init__(self):
@@ -113,9 +139,10 @@ class ViewWindow(Window,Win_frames):
                 except subprocess.CalledProcessError as e:
                     print(f"Error allowing port {port}: {e}")
 
-    def frames_pos(self):
+    def child_frames_pos(self):
+        self.main_frame_gen()
         self.text_list = ["Password Chk", "Blocking SSH"]
-        self.frames_list = self.frames_gen(2,1,400,400)
+        self.frames_list = self.child_frames_gen(2,1,400,400, "#080808")
         self.frames_list[0].pack(side="top", expand = False)
         self.frames_list[1].pack(side="top", expand = False)
         print("Frame positioned")
@@ -136,6 +163,8 @@ class ViewWindow(Window,Win_frames):
         pol = self.FirewallPolicy()
 
         print("Secure shell ssh")
+
+        """
         # Enable UFW
         pol.enable_ufw()
 
@@ -147,12 +176,13 @@ class ViewWindow(Window,Win_frames):
 
         # Allow custom ports
         pol.allow_custom_ports(custom_ports)
+        """
 
 
 if __name__=="__main__":
     #Pop-Up Configuration
     Preconfig().auto_config_tk()
-    AuthWindow().frames_pos()
+    AuthWindow().child_frames_pos()
     win.mainloop()
 
 
