be99adc62e99d651d0b3a9738f7ba38b59b70467
Tanir Sahoo <121476039+tanirsahoo@users.noreply.github.com>
2023-10-05 23:54:16 +0000

    Creating a new User
    This python file enables an admin to enter username and password as input and create a new user account from the details.

---
+++ Tanir Sahoo/create_user.py
@@ -0,0 +1,27 @@
+import getpass
+import subprocess
+
+def create_user(username, password):
+  """Creates a new user with the specified username and password.
+
+  Args:
+    username: The username of the new user.
+    password: The password of the new user.
+  """
+
+  # Create the new user.
+  subprocess.run(["sudo", "useradd", username])
+
+  # Set the password for the new user.
+  subprocess.run(["sudo", "passwd", "-p", password, username])
+
+if __name__ == "__main__":
+  # Get the username and password from the admin.
+  username = input("Enter the username of the new user: ")
+  password = getpass.getpass("Enter the password for the new user: ")
+
+  # Create the new user.
+  create_user(username, password)
+
+  # Print a success message.
+  print(f"User {username} created successfully.")
