f295530b71355f9d7006b13ffeee3286c541f892
SM-2102 <91632338+SM-2102@users.noreply.github.com>
2023-12-09 23:42:12 +0530

    lalala

---
+++ Sukanya Manna/decrypt.py
@@ -0,0 +1,23 @@
+import subprocess
+import getpass
+
+def decrypt_file_with_password(file_path, password):
+    # Decrypt the file using GPG symmetric decryption
+    command = f"gpg --decrypt --passphrase {password} --output {file_path[:-4]} {file_path}"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+
+    if process.returncode == 0:
+        print(f"File '{file_path}' decrypted successfully.")
+    else:
+        print("Error decrypting the file:", error.decode().strip())
+
+if __name__ == "__main__":
+    # Specify the encrypted file to be decrypted
+    encrypted_file_path = input("Enter file name : ").strip()
+
+    # Get the GPG password from the user
+    password = getpass.getpass("Enter the GPG password: ")
+
+    # Decrypt the file using the provided password
+    decrypt_file_with_password(encrypted_file_path, password)
---
+++ Sukanya Manna/encrypt_db.py
@@ -0,0 +1,41 @@
+
+import subprocess
+
+def generate_gpg_password():
+    # Generate a random password using GPG
+    command = "gpg --gen-random --armor 1 16"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    if process.returncode == 0:
+        return output.decode().strip()
+    else:
+        print("Error generating GPG password:", error.decode().strip())
+        return None
+
+def encrypt_file_with_password(file_path, password):
+    # Encrypt the file using GPG symmetric encryption
+    command = f"gpg --symmetric --passphrase {password} --output {file_path}.gpg {file_path}"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+
+    if process.returncode == 0:
+        print(f"File '{file_path}' encrypted successfully.")
+    else:
+        print("Error encrypting the file:", error.decode().strip())
+
+if __name__ == "__main__":
+    # Generate a GPG password
+    password = generate_gpg_password()
+
+    if password:
+        # Specify the file to be encrypted
+
+
+        file_path = input("Enter file name : ").strip()
+
+        # Encrypt the file using the generated password
+        encrypt_file_with_password(file_path, password)
+
+    
+
