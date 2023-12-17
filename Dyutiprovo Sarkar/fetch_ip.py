309d81316117e5d3ff4b31d0f1991877fa76ba54
Dyutiprovo <115351448+Dyutiprovo@users.noreply.github.com>
2023-12-18 04:05:59 +0530

    Update fetch_ip.py
    lol

--- Dyutiprovo Sarkar/fetch_ip.py
+++ Dyutiprovo Sarkar/fetch_ip.py
@@ -1,19 +1,29 @@
 import socket
+import requests
 
 def get_local_ip():
     try:
-        # Get the hostname of the local machine
-        host_name = socket.gethostname()
-        # Get the IP address associated with the hostname
-        ip_address = socket.gethostbyname(host_name)
-        return ip_address
-    except socket.error as e:
-        print(f"Error: {e}")
-        return None
+        # Create a socket object and connect to an external server (e.g., Google DNS)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        local_ip = s.getsockname()[0]
+        s.close()
+        return local_ip
+    except socket.error:
+        return "Unable to get local IP address."
+
+def get_public_ip():
+    try:
+        # Use a service like 'httpbin' to get the public IP address
+        response = requests.get('https://httpbin.org/ip')
+        public_ip = response.json()['origin']
+        return public_ip
+    except requests.RequestException:
+        return "Unable to get public IP address."
 
 if __name__ == "__main__":
     local_ip = get_local_ip()
-    if local_ip:
-        print(f"Local IP Address: {local_ip}")
-    else:
-        print("Unable to fetch the local IP address.")
+    public_ip = get_public_ip()
+
+    print(f"Local IP Address: {local_ip}")
+    print(f"Public IP Address: {public_ip}")
