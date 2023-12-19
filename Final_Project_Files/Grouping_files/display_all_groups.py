23c7a850f04588a0448355c731eab2b261698b48
Tanir Sahoo <121476039+tanirsahoo@users.noreply.github.com>
2023-12-19 13:11:49 +0530

    Updates

--- Final_Project_Files/Grouping_files/display_all_groups.py
+++ Final_Project_Files/Grouping_files/display_all_groups.py
@@ -1,16 +1,30 @@
 import grp
+import sqlite3
 
-def list_all_groups():
-    try:
-        groups = grp.getgrall()
-        print("All groups on the system:")
-        for group in groups:
-            print(group.gr_name)
-    except Exception as e:
-        print(f"Error listing groups: {e}")
+#def list_all_groups():
+    #try:
+        #groups = grp.getgrall()
+        #print("All groups on the system:")
+        #for group in groups:
+            #print(group.gr_name)
+    #except Exception as e:
+        #print(f"Error listing groups: {e}")
+
+def database_read():
+   conn = sqlite3.connect("shared_database.db")
+   c = conn.cursor()
+   query = "SELECT name FROM group_details;"
+   try:
+       c.execute(query)
+       data = c.fetchall()
+       return data
+       conn.close()
+   except sqlite3.Error as e:
+       print("There is some problem in displaying the groups created. The exception is: " , e)
 
 def main():
-    list_all_groups()
+    list_itm = database_read()
+    print(list_itm)
 
 if __name__ == "__main__":
     main()
--- Final_Project_Files/Grouping_files/remove_user_from_group.py
+++ Final_Project_Files/Grouping_files/remove_user_from_group.py
@@ -1,11 +1,31 @@
 import subprocess
+import sqlite3
 
 def remove_user_from_group(username, group_name):
     try:
         subprocess.run(['sudo', 'gpasswd', '-d', username, group_name], check=True)
+        database_read(group_name , username)
         print(f"User '{username}' removed from group '{group_name}' successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error removing user from group: {e}")
+
+def database_read(group_name , username):
+   conn = sqlite3.connect("shared_database.db")
+   c = conn.cursor()
+   query = f"SELECT users FROM group_details WHERE name='{group_name}';"
+   c.execute(query)
+   data = c.fetchall()
+   yom = data[0]
+   dta = yom[0]
+   print(type(dta))
+   print(dta)
+   itm_array = dta.split(',')
+   itm_array.remove(username)
+   result_string = ', '.join(itm_array)
+   query = f"UPDATE group_details SET users = {result_string} WHERE name = {username};"
+   c.execute(query)
+   #return data
+   conn.close()
 
 def main():
     username = input("Enter the username: ")
