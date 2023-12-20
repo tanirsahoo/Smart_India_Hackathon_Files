8d7e20ebf49523680979a3b2810e2dbb5ea856e6
Tanir Sahoo <121476039+tanirsahoo@users.noreply.github.com>
2023-12-20 16:25:58 +0530

    updates

--- Tanir/choose_script.sh
+++ Tanir/choose_script.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
-
+#demofile2.txt
 file_path="choice_script_file.txt"
 input_string=$(<"$file_path")
 IFS=','
 read -ra myArray <<< "$input_string"
-IFS=' '
-userResponse=("1.1.6" "1.7.5" "1.7.6")
+#IFS=' '
+#userResponse=("1.1.6" "1.7.5" "1.7.6")
+#userResponse=("0")
+#echo "${myArray[2]}"
 
-for element in "${userResponse[@]}"; do
-  if [ "$element" = "1.1.4" ]; then
+for element in "${myArray[@]}"; do
+  #echo "$element"
+  if [ "$element" = "1" ]; then
       source Hard_Script_1.1.4.sh
   fi
-  if [ "$element" = "1.1.5" ]; then
+  if [ "$element" = "2" ]; then
       source Hard_Script_1.1.5.sh
   fi
-  if [ "$element" = "1.1.6" ]; then
+  if [ "$element" = "3" ]; then
       source Hard_script_1.1.6.sh
   fi
-  if [ "$element" = "1.3.2" ]; then
+  if [ "$element" = "4" ]; then
       source Hard_Script_1.3.2.sh
   fi
-  if [ "$element" = "1.6.1.1" ]; then
+  if [ "$element" = "5" ]; then
       source Hard_Script_1.6.1.1.sh
   fi
-  if [ "$element" = "1.6.1.2" ]; then
+  if [ "$element" = "6" ]; then
       source Hard_Script_1.6.1.2.sh
   fi
-  if [ "$element" = "1.6.1.3" ]; then
+  if [ "$element" = "7" ]; then
       source Hard_Script_1.6.1.3.sh
   fi
-  if [ "$element" = "1.7.4" ]; then
+  if [ "$element" = "8" ]; then
       source Hard_Script_1.7.4.sh
   fi
-  if [ "$element" = "1.7.5" ]; then
+  if [ "$element" = "9" ]; then
       source Hard_Script_1.7.5.sh
   fi
-  if [ "$element" = "1.7.6" ]; then
+  if [ "$element" = "10" ]; then
       source Hard_Script_1.7.6.sh    
   fi
 done
